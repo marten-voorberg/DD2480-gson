ffcf2590915208ba38e05ff58f63c419e8d9e9ee
Marten Voorberg <martenvoorberg@gmail.com>
2023-03-02 14:08:30 +0100

    Test #43: Update JsonWriterTest#testStrictnessNull to use assertThrows

--- gson/src/test/java/com/google/gson/stream/JsonWriterTest.java
+++ gson/src/test/java/com/google/gson/stream/JsonWriterTest.java
@@ -70,9 +70,12 @@ public final class JsonWriterTest {
   @Test
   public void testSetStrictnessNull() throws IOException {
     JsonWriter jsonWriter = new JsonWriter(new StringWriter());
-    assertThrows(NullPointerException.class, () -> jsonWriter.setStrictness(null));
-    jsonWriter.value(false);
-    jsonWriter.close();
+    try {
+      assertThrows(NullPointerException.class, () -> jsonWriter.setStrictness(null));
+    } finally {
+      jsonWriter.value(false);
+      jsonWriter.close();
+    }
   }
 
   @Test
